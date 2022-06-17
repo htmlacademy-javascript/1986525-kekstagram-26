6797f89c8f8556b8d0079c0015ae5f611193520f
Keks <keks@htmlacademy.ru>
2022-06-17 07:30:33 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,23 @@
+// Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+  let random;
+  if (min >= 0 && max >= 0) {
+    if (min <= max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      random = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+  }
+  return random;
+}
+
+function verifyStrLength(str, maxLength) {
+  let verify = false;
+  if (str.length <= maxLength) {
+    verify = true;
+  }
+  return verify;
+}
+
+getRandomIntInclusive(0, 20);
+verifyStrLength('Какой-то текст...', 50);
