929a13bfe8b033f191e119de37e3512044f3f419
Keks <keks@htmlacademy.ru>
2022-07-27 16:57:13 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/util.js
@@ -0,0 +1,22 @@
+// Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const getRandomIntInclusive = (min, max) => {
+  let random;
+  if (min >= 0 && max >= 0) {
+    if (min <= max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      random = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+  }
+  return random;
+};
+
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export {getRandomIntInclusive, debounce};
